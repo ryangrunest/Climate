31f38ae2d4ae2095537424d3a4495b0835df1e2d
Ryan Grunest <ryangrunest45@gmail.com>
2019-05-30 13:39:25 -0700

    successful request to weather api

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,20 +1,16 @@
 var db = require("../models");
 
 module.exports = function(app) {
+
   // Load index page
   app.get("/", function(req, res) {
     res.render("index");
   });
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    // db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-    //   res.render("example", {
-    //     example: dbExample
-    //   });
-    // });
+  // Load city page
+  app.get("/:city", function(req, res) {
+    let city = req.params.city;
+    res.render("city", { city: city, message: 'Hello there!' });
   });
-
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
